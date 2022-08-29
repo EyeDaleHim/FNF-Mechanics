cf884397f9987a2d7027132c621d845e74af7ccb
Raltyro <78720179+Raltyro@users.noreply.github.com>
2022-08-29 12:19:17 +0700

    Create MP4Handler.hx

---
+++ source/vlc/MP4Handler.hx
@@ -0,0 +1,9 @@
+package vlc;
+
+#if (hxCodec < 2.6.0)
+#error "Please Use the git Version of hxCodec! read \"README.md\" for installation"
+#end
+
+import VideoHandler;
+
+typedef MP4Handler = VideoHandler;
